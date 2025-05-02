962b54b3ca248cc1d25be8363be00c1dab9cabce
Ferdi Ardiansa <ferdiardiansa@gmail.com>
2025-05-02 16:25:19 +0700

    feat: add button

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
 const path = require('path')
  
 const buildEslintCommand = (filenames) =>
@@ -6,5 +7,5 @@ const buildEslintCommand = (filenames) =>
     .join(' --file ')}`
  
 module.exports = {
-  '*.{js,jsx,ts,tsx}': [buildEslintCommand],
+  '*.{mjs,js,ts,tsx}': [buildEslintCommand],
 }
