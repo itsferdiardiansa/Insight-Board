c9fdb8de29afafde478d1106f4a7ef82c089b423
Ferdi Ardiansa <ferdiardiansa@gmail.com>
2025-06-20 22:21:46 +0700

    style: change font family

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import { PropsWithChildren } from 'react'
 import type { Viewport } from 'next'
-import { JetBrains_Mono, Source_Sans_3 } from 'next/font/google'
 
 import { defaultMetadata } from '@/config/site-metadata'
 import { AnimationProvider } from '@/context/gsap-animation'
@@ -9,18 +8,6 @@ import { websiteSchema } from '@/schema/website'
 import { homepageSchema } from '@/schema/homepage'
 
 import '@/styles/globals.css'
-
-const sourceSans = Source_Sans_3({
-  variable: '--font-source-sans',
-  subsets: ['latin'],
-  display: 'swap',
-})
-
-const jetBrainsMono = JetBrains_Mono({
-  variable: '--font-mono',
-  subsets: ['latin'],
-  display: 'swap',
-})
 
 export const viewport: Viewport = {
   width: 'device-width',
@@ -32,15 +19,12 @@ export const metadata = defaultMetadata
 
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
-    <html
-      lang="en"
-      className={`${sourceSans.variable} ${jetBrainsMono.variable}`}
-    >
+    <html lang="en">
       <head>
         <link rel="manifest" href="/manifest.json" />
         <JsonLd data={[websiteSchema, homepageSchema]} />
       </head>
-      <body className="antialiased font-sans text-neutral-800 bg-slate-50">
+      <body className="antialiased text-neutral-800 bg-slate-50">
         <AnimationProvider>{children}</AnimationProvider>
       </body>
     </html>
